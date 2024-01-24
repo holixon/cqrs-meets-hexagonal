729b76d931d633b504d174acad7fddc03edaa782
Jan Weinschenker <jan.weinschenker@holisticon.de>
2024-01-24 13:53:55 +0100

    chore: add spring initializr generated stuff

---
+++ src/main/kotlin/io/holixon/cqrshexagonaldemo/demoparent/DemoParentApplication.kt
@@ -0,0 +1,11 @@
+package io.holixon.cqrshexagonaldemo.demoparent
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoParentApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoParentApplication>(*args)
+}
---
+++ src/test/kotlin/io/holixon/cqrshexagonaldemo/demoparent/DemoParentApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.holixon.cqrshexagonaldemo.demoparent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoParentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
