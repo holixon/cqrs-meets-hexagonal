c9e533d4d7988bb04be464accc1d4e5919538360
Jan Weinschenker <jan.weinschenker@holisticon.de>
2024-01-25 16:06:58 +0100

    fix: unit test

--- demo-command/src/test/kotlin/io/holixon/cqrshexagonaldemo/demoparent/command/DemoParentApplicationTests.kt
+++ demo-command/src/test/kotlin/io/holixon/cqrshexagonaldemo/demoparent/command/DemoParentApplicationTests.kt
@@ -3,11 +3,11 @@ package io.holixon.cqrshexagonaldemo.demoparent.command
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+
 class DemoParentApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
