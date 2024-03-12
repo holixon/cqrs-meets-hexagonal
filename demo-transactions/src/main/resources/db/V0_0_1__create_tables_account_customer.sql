6409eb46787d1c0d2549103062026fc6ff80f691
rafaelpereira <rafael.pereira@holisticon.de>
2024-03-12 16:27:59 +0000

    Refactor: Add MySql docker configuration Refactor: Change flyway configuration to support jdbc database Refactor: Update dependencies to support mySql

--- demo-transactions/src/main/resources/db/V0_0_1__create_tables_account_customer.sql
+++ demo-transactions/src/main/resources/db/V0_0_1__create_tables_account_customer.sql
@@ -1,29 +1,15 @@
-CREATE SEQUENCE [transactions].account_seq
-    START WITH 1
-    INCREMENT BY 50
-GO
--- ------------------------------------------------------------------------------#
-CREATE SEQUENCE [transactions].customer_seq
-    START WITH 1
-    INCREMENT BY 50
-GO
--- ------------------------------------------------------------------------------#
-
--- ------------------------------------------------------------------------------#
-CREATE TABLE [transactions].account
-(
-    iban            VARCHAR(255) NOT NULL,
-    customer_number VARCHAR(255) NOT NULL,
-
-    CONSTRAINT PK_ACCOUNT_ID PRIMARY KEY (iban)
-)
-GO
--- ------------------------------------------------------------------------------#
-CREATE TABLE [transactions].customer
+CREATE TABLE transactions.customer
 (
     customer_number VARCHAR(255) NOT NULL,
     customer_name   VARCHAR(255) NOT NULL,
 
     CONSTRAINT PK_CUSTOMER_ID PRIMARY KEY (customer_number)
-)
-GO
+);
+
+CREATE TABLE transactions.account
+(
+    iban            VARCHAR(255) NOT NULL,
+    customer_number VARCHAR(255) NOT NULL,
+    CONSTRAINT FK_CustomerNumber FOREIGN KEY (customer_number) REFERENCES customer(customer_number),
+    CONSTRAINT PK_ACCOUNT_ID PRIMARY KEY (iban)
+);
